a9dc85f5675d12371025f219c5ff78277051d261
Arvind Iyer <arvindi.me@gmail.com>
2018-03-17 12:58:36 +0800

    Create match can pick time and sync with DB @TODO: Improve CreateMatch UI

--- app/src/main/java/com/apps/sloth/sportbuddy/CreateMatchActivity.kt
+++ app/src/main/java/com/apps/sloth/sportbuddy/CreateMatchActivity.kt
@@ -21,28 +21,48 @@ import java.util.*
 class CreateMatchActivity : AppCompatActivity() {
     private val currentUser = FirebaseAuth.getInstance().currentUser as FirebaseUser
     private val matchesDB = FirebaseDatabase.getInstance().getReference().child("matches")
-
+    private var hour : Int? = null
+    private var minute : Int? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_match)
+        val mCurrentTime = Calendar.getInstance()
+        val currenthour = mCurrentTime.get(Calendar.HOUR_OF_DAY)
+        val currentminute = mCurrentTime.get(Calendar.MINUTE)
+        cm_time_text.setText(currenthour.toString() + ":" + currentminute.toString())
     }
 
     fun createMatch(view: View) {
+        if(hour == null || minute == null) {
+            println("Set time first pl0x")
+            return
+        }
         val thisMatch = Match(
             sport= cm_sport.text.toString(),
             max_capacity= cm_max_capacity.text.toString().toInt(),
-            hourOfDay = 0,
-            minute = 0
+            hourOfDay = hour,
+            minute = minute,
+            host_id = currentUser.uid,
+            id = matchesDB.push().key
         )
-        thisMatch.host_id = currentUser.uid
-        thisMatch.id = matchesDB.push().key
         matchesDB.child(thisMatch.id).setValue(thisMatch)
         finish()
     }
 
     fun openTimePicker(view: View) {
-        val newFragment = TimePickerFragment()
-        newFragment.show(supportFragmentManager, "timePicker")
+        val mCurrentTime = Calendar.getInstance()
+        val currenthour = mCurrentTime.get(Calendar.HOUR_OF_DAY)
+        val currentminute = mCurrentTime.get(Calendar.MINUTE)
+
+        val mTimePickerDialog: TimePickerDialog
+        mTimePickerDialog = TimePickerDialog(view.context, TimePickerDialog.OnTimeSetListener {
+            timePicker, h, m ->
+                cm_time_text.setText(h.toString() + ":" + m.toString())
+                hour = h
+                minute = m
+        }, currenthour, currentminute, true)
+        mTimePickerDialog.setTitle("Select Time for Game")
+        mTimePickerDialog.show()
     }
 
 }
