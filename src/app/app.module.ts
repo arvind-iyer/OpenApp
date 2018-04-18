731cd5df903a492b05cca2cf7efad0670c1268c6
Arvind Iyer <arvindi.me@gmail.com>
2018-04-18 16:23:11 +0800

    Added base template for most pages
    @TODO: Connect to firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,99 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { ErrorHandler, NgModule } from '@angular/core';
-import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
-import { SplashScreen } from '@ionic-native/splash-screen';
-import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
+import { NgModule, ErrorHandler } from '@angular/core';
 
-import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home';
+import { IonicApp, IonicModule, IonicErrorHandler } from 'ionic-angular';
+
+import { InAppBrowser } from '@ionic-native/in-app-browser';
+import { SplashScreen } from '@ionic-native/splash-screen';
+
+import { IonicStorageModule } from '@ionic/storage';
+
+import { ConferenceApp } from './app.component';
+
+import { AboutPage } from '../pages/about/about';
+import { PopoverPage } from '../pages/about-popover/about-popover';
+import { AccountPage } from '../pages/account/account';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SchedulePage } from '../pages/schedule/schedule';
+import { ScheduleFilterPage } from '../pages/schedule-filter/schedule-filter';
+import { SessionDetailPage } from '../pages/session-detail/session-detail';
+import { SignupPage } from '../pages/signup/signup';
+import { MatchDetailPage } from '../pages/match-detail/match-detail';
+import { MatchListPage } from '../pages/match-list/match-list';
+import { TabsPage } from '../pages/tabs-page/tabs-page';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { SupportPage } from '../pages/support/support';
+
+import { ConferenceData } from '../providers/conference-data';
+import { UserData } from '../providers/user-data';
+
 
 @NgModule({
   declarations: [
-    MyApp,
-    HomePage
+    ConferenceApp,
+    AboutPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    PopoverPage,
+    SchedulePage,
+    ScheduleFilterPage,
+    SessionDetailPage,
+    SignupPage,
+    MatchDetailPage,
+    MatchListPage,
+    TabsPage,
+    TutorialPage,
+    SupportPage
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    HttpModule,
+    IonicModule.forRoot(ConferenceApp, {}, {
+      links: [
+        { component: TabsPage, name: 'TabsPage', segment: 'tabs-page' },
+        { component: SchedulePage, name: 'Schedule', segment: 'schedule' },
+        { component: SessionDetailPage, name: 'SessionDetail', segment: 'sessionDetail/:sessionId' },
+        { component: ScheduleFilterPage, name: 'ScheduleFilter', segment: 'scheduleFilter' },
+        { component: MatchListPage, name: 'MatchList', segment: 'matchList' },
+        { component: MatchDetailPage, name: 'MatchDetail', segment: 'matchDetail/:matchId' },
+        { component: MapPage, name: 'Map', segment: 'map' },
+        { component: AboutPage, name: 'About', segment: 'about' },
+        { component: TutorialPage, name: 'Tutorial', segment: 'tutorial' },
+        { component: SupportPage, name: 'SupportPage', segment: 'support' },
+        { component: LoginPage, name: 'LoginPage', segment: 'login' },
+        { component: AccountPage, name: 'AccountPage', segment: 'account' },
+        { component: SignupPage, name: 'SignupPage', segment: 'signup' }
+      ]
+    }),
+    IonicStorageModule.forRoot()
   ],
   bootstrap: [IonicApp],
   entryComponents: [
-    MyApp,
-    HomePage
+    ConferenceApp,
+    AboutPage,
+    AccountPage,
+    LoginPage,
+    MapPage,
+    PopoverPage,
+    SchedulePage,
+    ScheduleFilterPage,
+    SessionDetailPage,
+    SignupPage,
+    MatchDetailPage,
+    MatchListPage,
+    TabsPage,
+    TutorialPage,
+    SupportPage
   ],
   providers: [
-    StatusBar,
-    SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ConferenceData,
+    UserData,
+    InAppBrowser,
+    SplashScreen
   ]
 })
-export class AppModule {}
+export class AppModule { }
