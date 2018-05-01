b6e4da117fca3253a2136f24682eae4de8bdd0ab
Arvind Iyer <arvindi.me@gmail.com>
2018-05-02 00:09:47 +0800

    ignore

--- src/pages/match-detail/match-detail.ts
+++ src/pages/match-detail/match-detail.ts
@@ -1,16 +1,20 @@
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-
+import { Match } from '../../interfaces/match';
+import { LoginPage } from '../login/login';
 
 @Component({
   selector: 'page-match-detail',
   templateUrl: 'match-detail.html'
 })
 export class MatchDetailPage {
-  match: any;
+  match: Match;
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.match = navParams.data;
+    if (!this.match.start_time) {
+      navCtrl.push(LoginPage);
+    }
     console.log(this.match);
   }
 
