6854bb4f1a0a05fb46a48b36a4623528606a47aa
Arvind Iyer <arvindi.me@gmail.com>
2018-05-12 10:55:34 +0800

    give up on security

---
+++ src/providers/firebase/config.ts
@@ -0,0 +1,10 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyBjVDCO75CQBPuGDmlDapBIK-g1BAJL0Ew",
+    authDomain: "sportbuddy-dc1d6.firebaseapp.com",
+    databaseURL: "https://sportbuddy-dc1d6.firebaseio.com",
+    projectId: "sportbuddy-dc1d6",
+    storageBucket: "sportbuddy-dc1d6.appspot.com",
+    messagingSenderId: "853900390131"
+  };
+  
+  export { firebaseConfig };
