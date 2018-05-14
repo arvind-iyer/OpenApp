122b9003e49393c0352f84ae65a52cddda0f50dd
Arvind Iyer <arvindi.me@gmail.com>
2018-05-14 12:53:35 +0800

    Additional fixes to host name

--- src/pages/match-detail/match-detail.ts
+++ src/pages/match-detail/match-detail.ts
@@ -1,15 +1,17 @@
 import { FirebaseDatabase, FirebaseAuth } from './../../providers/firebase/firebase';
 import { Component } from '@angular/core';
 import { NavController, NavParams } from 'ionic-angular';
-import { Match } from '../../interfaces/match';
+import { Match, AboutSettings } from '../../interfaces/match';
 import { LoginPage } from '../login/login';
+
 @Component({
   selector: 'page-match-detail',
   templateUrl: 'match-detail.html'
 })
+
 export class MatchDetailPage {
   match: Match;
-
+  host: AboutSettings = {name: ""};
   constructor(public navCtrl: NavController, public navParams: NavParams, 
     private db: FirebaseDatabase,
     private au: FirebaseAuth) {
@@ -18,6 +20,10 @@ export class MatchDetailPage {
       navCtrl.push(LoginPage);
     }
     console.log(this.match.participants.length);
+    this.db.afd.object("users/" + this.match.participants[0] + "/about").valueChanges().subscribe(
+      data => {
+        this.host = data;
+      });
   }
 
   getTime(timestamp) {
@@ -28,9 +34,6 @@ export class MatchDetailPage {
       );
   }
 
-  hostName(match: Match) {
-    return this.db.afd.object("users/" + match.host_id + "/about/name").valueChanges();
-  } 
 
   getDate(timestamp) {
     var time = new Date(timestamp);
