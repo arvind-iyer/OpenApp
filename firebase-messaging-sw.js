258f155a9d203b31182ed76d619a0c234cf58a3c
Arvind Iyer <arvindi.me@gmail.com>
2018-05-20 19:39:47 +0800

    Pushing to github pages

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,20 @@
+importScripts('https://www.gstatic.com/firebasejs/5.0.3/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/5.0.3/firebase-messaging.js');
+
+//initiialize firebase
+
+firebase.initializeApp({
+    messagingSenderId: '853900390131'
+  });
+  
+  const messaging = firebase.messaging();
+
+  messaging.setBackgroundMessageHandler(function(payload) {
+    console.log('Received background message ', payload);
+    // here you can override some options describing what's in the message; 
+    // however, the actual content will come from the Webtask
+    const notificationOptions = {
+      icon: '/assets/icon.png'
+    };
+    return self.registration.showNotification(notificationTitle, notificationOptions);
+  });
